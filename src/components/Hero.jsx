a2498097c5ecc230d6fde080fcd59e726c47652b
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-08 16:36:11 +0700

    Update mobile layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,16 +7,16 @@ const Hero = () => {
 
     return (
         <header className='w-full flex justify-center items-center flex-col'>
-            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+            <nav className='flex justify-between items-center w-full mb-6 sm:mb-10 pt-3'>
                 <div className="flex justify-center items-center gap-0.5  hover:scale-105 transition hover:cursor-pointer">
-                    <img src={logo} alt='logo' className='w-12' />
-                    <p className="text-2xl text-center">AI-Summarizer</p>
+                    <img src={logo} alt='logo' className='w-10 sm:w-12' />
+                    <p className="text-md sm:text-2xl text-center">AI-Summarizer</p>
                 </div>
 
                 <div>
                     {/* ✅ Navigate to Font Showcase Page */}
                     <button
-                        className='text-sm hover:underline mr-5 hover:cursor-pointer underline-offset-2'
+                        className='text-[10px] sm:text-md hover:underline mr-1.5 sm:mr-4 hover:cursor-pointer underline-offset-2'
                         onClick={() => navigate("/FontPage")}
                     >
                         Font Showcase
